7a013954915c9c907e8c5f37a963ad3749a86012
vandero <robin.vandenbroeck@gmail.com>
2014-11-06 23:35:07 +0100

    - Added a splitter and menus to the main view

--- app/app.js
+++ app/app.js
@@ -5,12 +5,17 @@ var KINVEY_DEBUG = true;
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-    'ngResource',
+  'ngResource',
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
   'kendo.directives'
 ]).
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.controller('MyAppController', function($scope) {
+  $scope.loggedInUser = "robin";
+});
